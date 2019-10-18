20aa537560d1f73bb8ecc2e1786c8b94555d7b3d
Fletcher Collins <fletcher@colli.nz>
2019-10-17 22:35:59 -0600

    implement tests for h_rolling

--- test_hash_functions.py
+++ test_hash_functions.py
@@ -37,6 +37,46 @@ class TestHashFunctions(unittest.TestCase):
 
         self.assertEqual(strsum % randn, hf.h_ascii(randnum, randn))
 
+    def test_h_rolling_single_char(self):
+        randval = random.randint(32, 126)
+        randn = random.randint(1, 100)
+
+        s = ((randval * 53) % 2 ** 64)
+
+        self.assertEqual(s % randn, hf.h_rolling(str(chr(randval)), randn))
+
+    def test_h_rolling_random_str(self):
+        randstr = ""
+        randlen = random.randint(1, 20)
+        randn = random.randint(1, 100)
+        s = 0
+        for i in range(randlen):
+            randchar = random.randint(32, 126)
+            randstr += chr(randchar)
+            s += (randchar * 53 ** i)
+
+        s = (s % 2 ** 64)
+
+        self.assertEqual(s % randn, hf.h_rolling(randstr, randn))
+
+    def test_h_rolling_empty_str(self):
+        self.assertEqual(0, hf.h_rolling("", random.randint(1,100))) 
+
+    def test_h_rolling_invalid_n(self):
+        self.assertEqual(None, hf.h_rolling("whatever", random.randint(-100,0)))
+
+    def test_h_rolling_non_string_key(self):
+        randnum = random.randint(0,1000)
+        randn = random.randint(0,100) 
+        s = 0
+        for i in range(len(str(randnum))):
+            s += ord((str(randnum))[i])
+            s += (int(str(randnum)[i]) * 53 ** i)
+
+        s = (s % 2 ** 64)
+
+        self.assertEqual(s % randn, hf.h_rolling(randnum, randn))
+
 
 
 if __name__ == "__main__":
