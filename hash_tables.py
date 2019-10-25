86fc54fe9dd1f67a363ac63541242a4be2770742
Fletcher Collins <fletcher@colli.nz>
2019-10-24 19:42:55 -0600

    implement chained hash

--- hash_tables.py
+++ hash_tables.py
@@ -23,18 +23,25 @@ class LinearProbe:
         for entry in self.L[start_hash:]:
             if key == entry[0]:
                 return entry[1]
+        return None
 
 class ChainedHash:
     def __init__(self, N, hash_function):
         self.hash_function = hash_function
         self.N = N
 
+        self.L = [[]] * self.N
+
     def add(self, key, value):
         start_hash = self.hash_function(key, self.N)
-        pass
+
+        self.L[start_hash].append((key, value))
 
     def search(self, key):
         start_hash = self.hash_function(key, self.N)
-        pass
+        for entry in self.L[start_hash]:
+            if key == entry[0]:
+                return entry[1]
+        return None
 
 
